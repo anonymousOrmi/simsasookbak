0dfb541e900f433c6e5b3d6e234fac67f5d07a0f
www-author <dream970215@naver.com>
2024-04-29 11:25:54 +0900

    chore : 프로젝트 초기 빌드 파일 및 환경 설정

---
+++ src/main/java/com/simsasookbak/SimsasookbakApplication.java
@@ -0,0 +1,13 @@
+package com.simsasookbak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsasookbakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimsasookbakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simsasookbak/SimsasookbakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simsasookbak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsasookbakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
