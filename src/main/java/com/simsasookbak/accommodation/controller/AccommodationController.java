ed497eadc878430486efa447628de6008143ad25
BlueHarmel <dldjdtjr@naver.com>
2024-05-02 10:09:46 +0900

    Refactor: Controller RestController 분리

--- src/main/java/com/simsasookbak/accommodation/controller/AccommodationController.java
+++ src/main/java/com/simsasookbak/accommodation/controller/AccommodationController.java
@@ -4,25 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.simsasookbak.accommodation.dto.AccommodationDto;
-import com.simsasookbak.accommodation.service.AccommodationService;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
-//@RestController
 @Controller
-//@RequiredArgsConstructor
 @RequestMapping("accommodation")
 public class AccommodationController {
 
-    //    private final AccommodationService accommodationService;
+
     @GetMapping("/{acom_id}")
     public String details(@PathVariable Integer acom_id){
 
@@ -34,19 +23,8 @@ public class AccommodationController {
     }
     @PostMapping("/reservation/message")
     public void reservationMessage(){
-
     }
     @PostMapping("/{acom_id}/comment")
     public void review(@PathVariable Integer acom_id){
-
     }
-
-//    @GetMapping("/search")
-//    public ResponseEntity<?> searchAccommodation(@RequestParam("keyword") String keyword) {
-//        List<AccommodationDto> accommodations = accommodationService.searchAccommodation(keyword);
-//        if (accommodations.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("검색 내용이 없습니다.");
-//        }
-//        return ResponseEntity.ok().body(accommodations);
-//    }
 }
