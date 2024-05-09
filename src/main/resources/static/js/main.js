9ac14fa80624300afe153a41591033f817da2b1d
www-author <dream970215@naver.com>
2024-05-09 09:32:50 +0900

    refactor : main.js 내 youtube api 호출 분리

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -127,31 +127,5 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     }); 
-
-    /* 유투브 데이터 조회 */
-    $.ajax({
-        url: '/api/youtube',
-        method: 'GET',
-        dataType : 'json',
-        success: function (response) {
-            let html = '';
-            $.each(response, function (index, data) {
-                console.log('data', data);
-                let videoId = $(data).prop('videoId');
-                html += '<div class="col-sm-6 col-md-6 col-lg-4 col-xl-3">' +
-                    '<div class="ratio ratio-16x9">' +
-                        `<iframe src="https://www.youtube.com/embed/${videoId}" 
-                             title="YouTube video" allowfullscreen 
-                             th:if="${videoId != null}"></iframe>` +
-                    '</div></div>';
-            });
-            $('#youtube').html(html);
-        },
-        error: function (error) {
-            console.error('Youtube Error : ', error);
-            $('#youtube-container').html('');
-            alert(error.responseText);
-        }
-    });
 })(jQuery);
 
