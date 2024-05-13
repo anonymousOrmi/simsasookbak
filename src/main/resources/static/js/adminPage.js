df687925e86623eb0a49b14b4184a824d8c9bd3d
githyuniiee <bony9728@gmail.com>
2024-05-13 23:53:02 +0900

    comment: "adminPage 회원 탈퇴 기능 추가"

--- src/main/resources/static/js/adminPage.js
+++ src/main/resources/static/js/adminPage.js
@@ -1,8 +1,6 @@
-$('#deleteButton').on('click', function() {
-
-    var memberId = $(this).val();
+$(document).on('click', '.deleteButton', function() {
+    var memberId = parseInt($(this).val(), 10);
     console.log('Member ID:', memberId);
-
 
     // 서버로 회원 ID를 전송하여 삭제 요청을 합니다.
     $.ajax({
@@ -10,7 +8,10 @@ $('#deleteButton').on('click', function() {
         url: '/admin/delete?memberId=' + memberId,
         success: function(response) {
             // 삭제 요청이 성공적으로 처리되었음을 알립니다.
+            window.location.reload();
             console.log('Deleted successfully');
+
+
         },
         error: function(xhr, status, error) {
             // 삭제 요청이 실패한 경우에 대한 처리를 수행합니다.
