ec42cdf95fcfb9b8c8c5086a0a35fe13f1020609
aammddkkzxc <ideal0427@naver.com>
2024-05-15 10:42:01 +0900

    feat: 숙소, 객실 삭제 타임리프까지 완성

--- src/main/java/com/simsasookbak/room/dto/RoomDto.java
+++ src/main/java/com/simsasookbak/room/dto/RoomDto.java
@@ -15,22 +15,26 @@ import lombok.NoArgsConstructor;
 public class RoomDto {
 
     private Long roomId;
+    private Long memberId;
     private Long accommodationId;
     private String name;
     private Integer cost;
     private String content;
     private String useGuide;
     private List<String> facilityList;
+    private boolean isDeleted;
 
     public static RoomDto toDto(Room room, List<String> facilityList) {
         return RoomDto.builder()
                 .roomId(room.getId())
+                .memberId(room.getAccommodation().getMember().getId())
                 .accommodationId(room.getAccommodation().getId())
                 .name(room.getName())
                 .cost(room.getCost())
                 .content(room.getContent())
                 .useGuide(room.getUseGuide())
                 .facilityList(facilityList)
+                .isDeleted(room.getIsDeleted())
                 .build();
     }
 
