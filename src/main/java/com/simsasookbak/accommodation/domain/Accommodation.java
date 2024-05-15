86e2837d0b997f6ccd71f54182a02a88c43990d1
githyuniiee <bony9728@gmail.com>
2024-05-16 03:04:02 +0900

    refactor : "숙소 엔티티 수정"

--- src/main/java/com/simsasookbak/accommodation/domain/Accommodation.java
+++ src/main/java/com/simsasookbak/accommodation/domain/Accommodation.java
@@ -44,10 +44,10 @@ public class Accommodation extends BaseEntity {
     @Column(name = "content", length = 1000, nullable = false)
     private String content;
 
-    @Column(name = "region", length = 10, nullable = false)
+    @Column(name = "region", length = 100, nullable = false)
     private String region;
 
-    @Column(name = "address", length = 20, nullable = false)
+    @Column(name = "address", length = 100, nullable = false)
     private String address;
 
     @Column(name = "check_in", nullable = false)
