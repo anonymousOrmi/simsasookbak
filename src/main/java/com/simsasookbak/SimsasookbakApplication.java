9c18d773fd2dbec472b2dc58d4483c55044c618b
aammddkkzxc <ideal0427@naver.com>
2024-05-16 17:30:33 +0900

    refactor: 주석제거

--- src/main/java/com/simsasookbak/SimsasookbakApplication.java
+++ src/main/java/com/simsasookbak/SimsasookbakApplication.java
@@ -1,19 +1,10 @@
 package com.simsasookbak;
 
-import com.simsasookbak.global.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-//<<<<<<< HEAD
-//@EnableAsync
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-////@SpringBootApplication
-//=======
 @SpringBootApplication
 public class SimsasookbakApplication {
 
