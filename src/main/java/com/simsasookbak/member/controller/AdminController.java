3d5ae53b2e6eccfc8ecf99bede362ac52f6bf1cb
aammddkkzxc <ideal0427@naver.com>
2024-05-16 20:14:02 +0900

    fix: delete요청으로 새로고침하기

--- src/main/java/com/simsasookbak/member/controller/AdminController.java
+++ src/main/java/com/simsasookbak/member/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -62,9 +63,9 @@ public class AdminController {
     }
 
     @DeleteMapping("/delete")
-    public String deleteUser(@RequestParam("memberId") long memberId) {
+    public ResponseEntity<Void> deleteUser(@RequestParam("memberId") long memberId) {
         adminService.deleteMember(memberId);
-        return "redirect:/admin/getAllMember"; // 관리자 페이지로 리다이렉트
+        return ResponseEntity.noContent().build(); // 관리자 페이지로 리다이렉트
     }
 
     @PostMapping("/updateRole")
